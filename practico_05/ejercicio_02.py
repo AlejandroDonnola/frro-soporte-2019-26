3ff3413315f52f006f2169e5989c01d875c7692a
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2019-07-03 19:09:33 -0300

    Update ejercicio_02.py

--- practico_05/ejercicio_02.py
+++ practico_05/ejercicio_02.py
@@ -23,42 +23,37 @@ class DatosSocio(object):
         return oSoc
 
     def todos(self):
-        """
-        Devuelve listado de todos los socios en la base de datos.
-        :rtype: list
-        """
-        return []
+        socios= self.session.query(Socio).all()
+        return socios
 
     def borrar_todos(self):
-        """
-        Borra todos los socios de la base de datos.
-        Devuelve True si el borrado fue exitoso.
-        :rtype: bool
-        """
-        return False
+        self.session.query(Socio).delete()
+        self.session.commit()
+        return True
 
     def alta(self, socio):
-        self.session.add(socio)
+        oSoc = Socio(DNI=socio.DNI, Nombre=socio.Nombre, Apellido=socio.Apellido)
+        self.session.add(oSoc)
         self.session.commit()
-        return socio
+        return oSoc
 
     def baja(self, id_socio):
-        oSoc = self.session.query(Socio).filter(Socio.IdSocio == id_socio).first()
-        if oSoc is None:
-            return False
-        else:
+        oSoc = self.buscar(id_socio)
+        if oSoc is not None:
             self.session.delete(oSoc)
             self.session.commit()
             return True
+        else:
+            return False
 
     def modificacion(self, socio):
-        """
-        Guarda un socio con sus datos modificados.
-        Devuelve el Socio modificado.
-        :type socio: Socio
-        :rtype: Socio
-        """
-        return socio
+        oSoc = self.buscar(socio.IdSocio)
+        if oSoc is not None:
+            oSoc.Nombre = socio.Nombre
+            oSoc.Apellido = socio.Apellido
+            oSoc.DNI = socio.DNI
+            self.session.commit()
+        return oSoc
 
 
 def pruebas():
@@ -66,7 +61,7 @@ def pruebas():
     datos = DatosSocio()
     socio = datos.alta(Socio(DNI=12345678, Nombre='Juan', Apellido='Perez'))
     assert socio.IdSocio > 0
-
+    
     # baja
     assert datos.baja(socio.IdSocio) is True
 
@@ -78,16 +73,16 @@ def pruebas():
     assert datos.buscar_dni(socio_2.DNI) == socio_2
 
     # modificacion
-    socio_3 = datos.alta(Socio(dni=12345680, nombre='Susana', apellido='Gimenez'))
-    socio_3.nombre = 'Moria'
-    socio_3.apellido = 'Casan'
-    socio_3.dni = 13264587
+    socio_3 = datos.alta(Socio(DNI=12345680, Nombre='Susana', Apellido='Gimenez'))
+    socio_3.Nombre = 'Moria'
+    socio_3.Apellido = 'Casan'
+    socio_3.DNI = 13264587
     datos.modificacion(socio_3)
-    socio_3_modificado = datos.buscar(socio_3.id)
-    assert socio_3_modificado.id == socio_3.id
-    assert socio_3_modificado.nombre == 'Moria'
-    assert socio_3_modificado.apellido == 'Casan'
-    assert socio_3_modificado.dni == 13264587
+    socio_3_modificado = datos.buscar(socio_3.IdSocio)
+    assert socio_3_modificado.IdSocio == socio_3.IdSocio
+    assert socio_3_modificado.Nombre == 'Moria'
+    assert socio_3_modificado.Apellido == 'Casan'
+    assert socio_3_modificado.DNI == 13264587
 
     # todos
     assert len(datos.todos()) == 2
