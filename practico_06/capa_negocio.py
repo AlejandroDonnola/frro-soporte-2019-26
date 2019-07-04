1c9ac4212974eb345cab157304ce79ccdcf24b63
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2019-07-04 15:26:25 -0300

    Revert "Update capa_negocio.py"
    This reverts commit bdcb378ac12680bef173ae6e8c2e93e20df79549.

--- practico_06/capa_negocio.py
+++ practico_06/capa_negocio.py
@@ -1,7 +1,7 @@
 # Implementar los metodos de la capa de negocio de socios.
 
-from Practica05.ejercicio_01 import Socio
-from Practica05.ejercicio_02 import DatosSocio
+from practico_05.ejercicio_01 import Socio
+from practico_05.ejercicio_02 import DatosSocio
 
 
 class DniRepetido(Exception):
@@ -26,49 +26,29 @@ class NegocioSocio(object):
         self.datos = DatosSocio()
 
     def buscar(self, id_socio):
-        oSoc=self.datos.buscar(id_socio)
-
-        return oSoc
+        """
+        Devuelve la instancia del socio, dado su id.
+        Devuelve None si no encuentra nada.
+        :rtype: Socio
+        """
+        return
 
     def buscar_dni(self, dni_socio):
-        oSoc=self.datos.buscar_dni(dni_socio)
-        if oSoc is not None:
-            return oSoc
-        else:
-            return None
-
-
+        """
+        Devuelve la instancia del socio, dado su dni.
+        Devuelve None si no encuentra nada.
+        :rtype: Socio
+        """
+        return
 
     def todos(self):
-        socios=self.datos.todos()
-        return socios
+        """
+        Devuelve listado de todos los socios.
+        :rtype: list
+        """
+        return []
 
     def alta(self, socio):
-        try:
-            self.regla_1(socio)
-        except DniRepetido as e:
-            print('Error:',e.args)
-            return False
-
-        try:
-            self.regla_2(socio)
-        except LongitudInvalida as e:
-            print('Error:',e.args)
-            return False
-
-        try:
-            self.regla_3(socio)
-        except MaximoAlcanzado as e:
-            print('Error:',e.args)
-            return False
-
-        else:
-            self.datos.alta(socio)
-            return True
-
-
-
-
         """
         Da de alta un socio.
         Se deben validar las 3 reglas de negocio primero.
@@ -80,22 +60,14 @@ class NegocioSocio(object):
         return False
 
     def baja(self, id_socio):
-        respuesta=self.datos.baja(id_socio)
-        return respuesta
+        """
+        Borra el socio especificado por el id.
+        Devuelve True si el borrado fue exitoso.
+        :rtype: bool
+        """
+        return False
 
     def modificacion(self, socio):
-        try:
-            self.regla_2(socio)
-        except LongitudInvalida as e:
-            print('Error:',e.args)
-            return False
-        else:
-            self.datos.alta(socio)
-            return True
-
-
-
-
         """
         Modifica un socio.
         Se debe validar la regla 2 primero.
@@ -104,56 +76,30 @@ class NegocioSocio(object):
         :type socio: Socio
         :rtype: bool
         """
+        return False
 
     def regla_1(self, socio):
-        oSoc = self.datos.buscar_dni(socio.DNI)
-        if oSoc is not None:
-            raise DniRepetido('DNI repetido')
-            return False
-        else:
-            return True
-
-
         """
         Validar que el DNI del socio es unico (que ya no este usado).
         :type socio: Socio
         :raise: DniRepetido
         :return: bool
         """
-
         return False
 
     def regla_2(self, socio):
-        x= len(socio.Nombre)
-        y= len(socio.Apellido)
-        if (x >= self.MIN_CARACTERES and x <= self.MAX_CARACTERES) and (y >=self.MIN_CARACTERES and y <= self.MAX_CARACTERES) :
-            return True
-        else:
-            raise LongitudInvalida('Nombre o Apellido no tienen entre 3 y 15 caracteres')
-            return false
-
-
-
         """
         Validar que el nombre y el apellido del socio cuenten con mas de 3 caracteres pero menos de 15.
         :type socio: Socio
         :raise: LongitudInvalida
         :return: bool
         """
-
+        return False
 
     def regla_3(self):
-        socios=self.datos.todos()
-        x=len(socios)
-        if  self.MAX_SOCIOS>x:
-            return False
-        else:
-            raise MaximoAlcanzado('Se esta excediendo la cantidad maxima de socios')
-            return True
-
         """
         Validar que no se esta excediendo la cantidad maxima de socios.
         :raise: MaximoAlcanzado
         :return: bool
         """
-
+        return False
